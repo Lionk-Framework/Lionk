d9cbb180c257f50d9019e6bb6c11eadc23edf9df
AlexandreIorio <alexandre.iorio@heig-vd.ch>
2024-07-13 22:46:12 +0200

    Work on nuget workflow

--- .github/workflows/scripts/revert_commit_csproj.py
+++ .github/workflows/scripts/revert_commit_csproj.py
@@ -1,7 +1,16 @@
 import os
 import subprocess
 
+def get_commit_count():
+    result = subprocess.run(["git", "rev-list", "--count", "HEAD"], capture_output=True, text=True)
+    return int(result.stdout.strip())
+
 def revert_last_commit():
+    commit_count = get_commit_count()
+    if commit_count <= 1:
+        print("Not enough commits to revert. Skipping revert.")
+        return
+
     try:
         # Revert the last commit
         subprocess.run(["git", "reset", "--hard", "HEAD~1"], check=True)
