cea6e1e93dbc048ef6c471b310955c54987cd8b2
Alexandre Iorio <alexandre.iorio@heig-vd.ch>
2024-07-15 17:00:41 +0200

    Work on app workflow

--- .github/workflows/scripts/app_scripts/backup_csproj.py
+++ .github/workflows/scripts/app_scripts/backup_csproj.py
@@ -2,15 +2,10 @@ import os
 # get environment variables
 app_path = os.getenv("APP_PATH")
 
+backup_file = os.path.join(app_path,".bkp")
 
-#  get project names from projects.txt
-with open(app_path, "r") as file:
-    app_content = file.read().strip().split()
+with open(app_path, "rb") as src:
+    content = src.read()
 
-    backup_file = os.path.join(app_path,".bkp")
-
-    with open(app_path, "rb") as src:
-        content = src.read()
-
-    with open(backup_file, "wb") as bkp:
-        bkp.write(content)
+with open(backup_file, "wb") as bkp:
+    bkp.write(content)
