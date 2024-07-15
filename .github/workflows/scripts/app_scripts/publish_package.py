df5d34dfc3c5f48f904f8c2dfad8cc0e5ad373de
AlexandreIorio <alexandre.iorio@heig-vd.ch>
2024-07-15 20:58:12 +0200

    Work on workflow

---
+++ .github/workflows/scripts/app_scripts/publish_package.py
@@ -0,0 +1,38 @@
+import subprocess
+import sys
+import os
+
+def run_command(command):
+    try:
+        result = subprocess.run(command, check=True, capture_output=True, text=True)
+        print("STDOUT:", result.stdout.strip())
+        print("STDERR:", result.stderr.strip())
+    except subprocess.CalledProcessError as e:
+        print(f"Error running command {command}: {e}")
+        print("STDOUT:", e.stdout)
+        print("STDERR:", e.stderr)
+        sys.exit(1)
+
+app_path = os.getenv('APP_PATH')
+app_name = os.getenv('APP_NAME')
+docker_registry = os.getenv('DOCKER_REGISTRY')
+gh_token = os.getenv('GH_TOKEN')
+
+with open('newversion.txt', 'r') as file:
+    newversion = file.read().strip()
+
+with open('description.txt', 'r') as file:
+    description = file.read().strip()
+
+print(f"Publishing {app_name} as version {newversion}")
+
+# Construire l'image Docker avec une étiquette de description
+run_command(['docker', 'build', '-t', f"{docker_registry}/{app_name}:{newversion}", '--label', f"description={description}", app_path])
+
+# Se connecter au registre Docker
+run_command(['echo', gh_token, '|', 'docker', 'login', docker_registry, '-u', 'USERNAME', '--password-stdin'])
+
+# Pousser l'image Docker au registre
+run_command(['docker', 'push', f"{docker_registry}/{app_name}:{newversion}"])
+run_command(['docker', 'push', f"{docker_registry}/{app_name}:latest"])
+exit(1)
---
+++ .github/workflows/scripts/app_scripts/restore_release.py
@@ -0,0 +1,25 @@
+import subprocess
+import sys
+import os
+
+app_name = os.getenv('APP_NAME')
+github_token = os.getenv('GITHUB_TOKEN')
+
+with open('newversion.txt', 'r') as file:
+    newversion = file.read().strip()
+
+
+tag = f"{app_name}_{newversion}"
+
+print(f"Deleting tag {tag}")
+
+# Delete the tag locally
+subprocess.run(['git', 'tag', '-d', tag], check=True)
+
+# Delete the tag remotely
+subprocess.run(['git', 'push', '--delete', 'origin', tag], check=True)
+
+# Delete the release
+subprocess.run(['gh', 'release', 'delete', tag, '--yes'], check=True)
+
+
