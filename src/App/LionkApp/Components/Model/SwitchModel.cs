d18df5fc17801980e4aedbf6e8905d82a4667167
Theodros Mulugeta <t.mulugeta@nextkidney.com>
2024-07-27 18:53:55 +0200

    Adding dynamic siwtch list

---
+++ src/App/LionkApp/Components/Model/SwitchModel.cs
@@ -0,0 +1,19 @@
+﻿// Copyright © 2024 Lionk Project
+
+namespace LionkApp.Components.Model;
+
+/// <summary>
+/// Model class for a switch component.
+/// </summary>
+public class SwitchModel
+{
+    /// <summary>
+    /// Gets or sets the switch label.
+    /// </summary>
+    public string Label { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the switch is enabled.
+    /// </summary>
+    public bool IsEnabled { get; set; } = true;
+}
