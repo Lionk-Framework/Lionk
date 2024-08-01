b0c9e6467a5a9c284f0ff3102432bb5714ed6a1c
CoJaques <colin.jaques@heig-vd.ch>
2024-08-01 13:36:27 +0200

    Add test concerning Logger

---
+++ src/LionkTest/Logger/LogServiceTests.cs
@@ -0,0 +1,70 @@
+﻿// Copyright © 2024 Lionk Project
+
+using Lionk.Log;
+using Moq;
+
+namespace LionkTest;
+
+/// <summary>
+/// Test class for <see cref="LogService"/>.
+/// </summary>
+public class LogServiceTests
+{
+    /// <summary>
+    /// Test for <see cref="LogService.Configure(ILoggerFactory)"/>.
+    /// </summary>
+    [Test]
+    public void Configure_ShouldInitializeLoggers()
+    {
+        var mockLoggerFactory = new Mock<ILoggerFactory>();
+        var mockAppLogger = new Mock<IStandardLogger>();
+        var mockDebugLogger = new Mock<IStandardLogger>();
+
+        mockLoggerFactory.Setup(factory => factory.CreateLogger("app")).Returns(mockAppLogger.Object);
+        mockLoggerFactory.Setup(factory => factory.CreateLogger("debug")).Returns(mockDebugLogger.Object);
+
+        LogService.Configure(mockLoggerFactory.Object);
+
+        Assert.That(LogService.CreateLogger("app"), Is.Not.Null);
+        Assert.That(LogService.CreateLogger("debug"), Is.Not.Null);
+    }
+
+    /// <summary>
+    /// Test for <see cref="LogService.LogDebug(string)"/>.
+    /// </summary>
+    [Test]
+    public void LogDebug_ShouldCallDebugLogger()
+    {
+        var mockLoggerFactory = new Mock<ILoggerFactory>();
+        var mockDebugLogger = new Mock<IStandardLogger>();
+
+        mockLoggerFactory.Setup(factory => factory.CreateLogger("debug")).Returns(mockDebugLogger.Object);
+        LogService.Configure(mockLoggerFactory.Object);
+
+        string message = "Debug message";
+
+        LogService.LogDebug(message);
+
+        mockDebugLogger.Verify(logger => logger.Log(LogSeverity.Debug, message), Times.Once);
+    }
+
+    /// <summary>
+    /// Test for <see cref="LogService.LogApp(LogSeverity, string)"/>.
+    /// </summary>
+    [Test]
+    public void LogApp_ShouldCallAppLogger()
+    {
+        var mockLoggerFactory = new Mock<ILoggerFactory>();
+        var mockAppLogger = new Mock<IStandardLogger>();
+
+        mockLoggerFactory.Setup(factory => factory.CreateLogger("app")).Returns(mockAppLogger.Object);
+        LogService.Configure(mockLoggerFactory.Object);
+
+        LogSeverity severity = LogSeverity.Information;
+        string message = "App message";
+
+        LogService.LogApp(severity, message);
+
+        mockAppLogger.Verify(logger => logger.Log(severity, message), Times.Once);
+    }
+}
---
+++ src/LionkTest/Logger/Serilog/SerilogLoggerTests.cs
@@ -0,0 +1,65 @@
+﻿// Copyright © 2024 Lionk Project
+
+using Lionk.Log;
+using Lionk.Log.Serilog;
+
+namespace LionkTest;
+
+/// <summary>
+/// Test class for <see cref="SerilogLogger"/>.
+/// </summary>
+public class SerilogLoggerTests
+{
+    /// <summary>
+    /// Method to test <see cref="SerilogLogger.Log(LogSeverity, string)"/>.
+    /// </summary>
+    [Test]
+    public void Log_ShouldWriteLogToFile()
+    {
+        string loggerName = "test";
+
+        string path = Path.GetFullPath(
+            Utils.DirectoryPath
+            + $"\\{loggerName}" +
+            DateOnly.FromDateTime(DateTime.Now).ToString("yyyyMMdd")
+            + $"{Utils.LogExtension}");
+
+        if (File.Exists(path))
+            File.Delete(path);
+
+        IStandardLogger logger = new SerilogFactory().CreateLogger(loggerName);
+        LogSeverity severity = LogSeverity.Information;
+        string message = "Test log message";
+
+        logger.Log(severity, message);
+        logger.Dispose();
+
+        Assert.That(File.Exists(path), Is.True);
+        string logContents = File.ReadAllText(path);
+        Assert.That(logContents.Contains("Test log message"), Is.True);
+    }
+
+    /// <summary>
+    /// Method to test <see cref="SerilogLogger.Log(LogSeverity, string)"/>.
+    /// </summary>
+    [Test]
+    public void CreateLogger_ShouldCreateFileLogger()
+    {
+        var factory = new SerilogFactory();
+        string loggerName = "custom";
+        string path = Path.GetFullPath(
+            Utils.DirectoryPath
+            + $"\\{loggerName}" +
+            DateOnly.FromDateTime(DateTime.Now).ToString("yyyyMMdd")
+            + $"{Utils.LogExtension}");
+
+        if (File.Exists(path))
+            File.Delete(path);
+
+        var logger = factory.CreateLogger(loggerName) as SerilogLogger;
+        logger?.Log(LogSeverity.Information, "Testing custom file logger");
+
+        Assert.That(logger, Is.Not.Null);
+        Assert.That(File.Exists(path), Is.True);
+    }
+}
