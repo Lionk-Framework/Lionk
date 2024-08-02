2ec491c6e7b4304dd9c3f3f1d3762bf1fb51f280
CoJaques <colin.jaques@heig-vd.ch>
2024-08-02 14:41:07 +0200

    Implement TypeProvider interface

---
+++ src/Lionk.Core/Model/TypesProvider/TypesEventArgs.cs
@@ -0,0 +1,21 @@
+﻿// Copyright © 2024 Lionk Project
+
+namespace Lionk.Core;
+
+/// <summary>
+/// Provides data for the TypeAvailable event.
+/// </summary>
+public class TypesEventArgs : EventArgs
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TypesEventArgs"/> class.
+    /// </summary>
+    /// <param name="type">The new types that is available.</param>
+    public TypesEventArgs(IEnumerable<Type> type)
+        => Types = type ?? throw new ArgumentNullException(nameof(type));
+
+    /// <summary>
+    /// Gets the new types that is available.
+    /// </summary>
+    public IEnumerable<Type> Types { get; }
+}
