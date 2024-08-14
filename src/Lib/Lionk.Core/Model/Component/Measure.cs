5cac27f3c33708af05354697c087b9334abdcf12
Alexandre Iorio <alexandre.iorio@heig-vd.ch>
2024-08-14 13:19:45 +0200

    Add measureName for Measure.cs

--- src/Lib/Lionk.Core/Model/Component/Measure.cs
+++ src/Lib/Lionk.Core/Model/Component/Measure.cs
@@ -9,6 +9,11 @@ namespace Lionk.Core.Component;
 public class Measure<T>
 {
     /// <summary>
+    /// Gets the name of the measure.
+    /// </summary>
+    public string MeasureName { get; private set; }
+
+    /// <summary>
     /// Gets the time of the measure.
     /// </summary>
     public DateTime Time { get; private set; }
@@ -26,21 +31,24 @@ public class Measure<T>
     /// <summary>
     /// Initializes a new instance of the <see cref="Measure{T}"/> class.
     /// </summary>
+    /// <param name="measureName"> The name of the measure. </param>
     /// <param name="unit"> The unit of the value. </param>
     /// <param name="value"> The value of the measure. </param>
-    public Measure(string unit, T value)
-        : this(DateTime.Now, unit, value)
+    public Measure(string measureName, string unit, T value)
+        : this(measureName, DateTime.Now, unit, value)
     {
     }
 
     /// <summary>
     /// Initializes a new instance of the <see cref="Measure{T}"/> class.
     /// </summary>
+    /// <param name="measureName"> The name of the measure. </param>
     /// <param name="time"> The time of the measure. </param>
     /// <param name="unit"> The unit of the value. </param>
     /// <param name="value"> The value of the measure. </param>
-    public Measure(DateTime time, string unit, T value)
+    public Measure(string measureName,  DateTime time, string unit, T value)
     {
+        MeasureName = measureName;
         Time = time;
         Unit = unit;
         Value = value;
