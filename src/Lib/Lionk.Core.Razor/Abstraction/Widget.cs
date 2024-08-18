a2c07bbc0d917b563e3533633981bb09825fea95
Alexandre Iorio <alexandre.iorio@heig-vd.ch>
2024-08-18 12:23:27 +0200

    rework widget

--- src/Lib/Lionk.Core.Razor/Abstraction/Widget.cs
+++ src/Lib/Lionk.Core.Razor/Abstraction/Widget.cs
@@ -7,13 +7,8 @@ namespace Lionk.Core.Component;
 /// <summary>
 /// This interface is used to define an element that can be used as a widget.
 /// </summary>
-public abstract class Widget : ComponentBase, IComponent
+public abstract class Widget : ComponentBase
 {
-    /// <summary>
-    /// Gets or sets the name of the widget.
-    /// </summary>
-    public string? InstanceName { get; set; }
-
     /// <summary>
     /// Gets or sets the component to display in the widget.
     /// </summary>
@@ -27,28 +22,11 @@ public abstract class Widget : ComponentBase, IComponent
     public IComponent? ComponentParameter { get; set; }
 
     /// <summary>
-    /// Gets or sets the name of the widget.
-    /// </summary>
-    /// <remarks> This property must have [Parameter] attribute. </remarks>
-    [Parameter]
-    public string? InstanceNameParameter
-    {
-        get;
-        set;
-    }
-
-    /// <summary>
-    /// Gets the type of the component that will be displayed in the widget.
-    /// </summary>
-    public abstract Type? ComponentType { get; }
-
-    /// <summary>
     /// This method is called when the component is initialized.
     /// </summary>
     protected override void OnInitialized()
     {
         base.OnInitialized();
         if (ComponentParameter != null) Component = ComponentParameter;
-        if (InstanceNameParameter != null) InstanceName = InstanceNameParameter;
     }
 }
