c912ae82c20bfd0e51bb960c50a5b137ffadf3fc
Alexandre Iorio <alexandre.iorio@heig-vd.ch>
2024-08-18 12:27:02 +0200

    rework view

---
+++ src/Lib/Lionk.Core.Razor/Abstraction/IWidgetable.cs
@@ -0,0 +1,15 @@
+﻿// Copyright © 2024 Lionk Project
+
+namespace Lionk.Core.Component;
+
+/// <summary>
+/// This interface is used to define an element that can be configured.
+/// </summary>
+public interface IWidgetable
+{
+    /// <summary>
+    /// This method is used to open the view of the component.
+    /// </summary>
+    /// <returns> A type that represents the view of the component. </returns>
+    Type? GetView();
+}
