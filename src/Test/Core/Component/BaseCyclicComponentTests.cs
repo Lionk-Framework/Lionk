9ade5f1062c4f9a7f758c51dd3576b0c6114d96c
CoJaques <colin.jaques@heig-vd.ch>
2024-08-20 14:27:46 +0200

    Add tests for base components class

---
+++ src/Test/Core/Component/BaseCyclicComponentTests.cs
@@ -0,0 +1,90 @@
+﻿// Copyright © 2024 Lionk Project
+
+using Lionk.Core.Component;
+
+namespace LionkTest.Core;
+
+/// <summary>
+/// Test class for the <see cref="BaseCyclicComponent"/> class.
+/// </summary>
+[TestFixture]
+public class BaseCyclicComponentTests
+{
+    private TestableCyclicComponent _cyclicComponent;
+
+    /// <summary>
+    /// Tears down the test environment.
+    /// </summary>
+    [TearDown]
+    public void TearDown()
+        => _cyclicComponent.Dispose();
+
+    /// <summary>
+    /// Setups the test environment.
+    /// </summary>
+    [SetUp]
+    public void Setup()
+        => _cyclicComponent = new TestableCyclicComponent();
+
+    /// <summary>
+    /// Tests that the <see cref="BaseCyclicComponent.OnInitialize"/> method sets the <see cref="BaseCyclicComponent.StartedDate"/> property correctly.
+    /// </summary>
+    [Test]
+    public void OnInitialize_SetsStartedDate()
+    {
+        DateTime beforeInitialization = DateTime.Now;
+
+        _cyclicComponent.InvokeOnInitialize();
+
+        Assert.That(_cyclicComponent.StartedDate, Is.GreaterThanOrEqualTo(beforeInitialization));
+    }
+
+    /// <summary>
+    /// Tests that the <see cref="BaseCyclicComponent.OnExecute(CancellationToken)"/> method increments the <see cref="BaseCyclicComponent.NbCycle"/> property.
+    /// </summary>
+    [Test]
+    public void OnExecute_IncrementsNbCycle()
+    {
+        _cyclicComponent.InvokeOnInitialize();
+        int initialNbCycle = _cyclicComponent.NbCycle;
+
+        _cyclicComponent.InvokeOnExecute(CancellationToken.None);
+
+        Assert.That(_cyclicComponent.NbCycle, Is.EqualTo(initialNbCycle + 1));
+    }
+
+    /// <summary>
+    /// Tests that the <see cref="BaseCyclicComponent.OnTerminate"/> method sets the <see cref="BaseCyclicComponent.LastExecution"/> property to the current date and time.
+    /// </summary>
+    [Test]
+    public void OnTerminate_SetsLastExecutionToNow()
+    {
+        _cyclicComponent.InvokeOnInitialize();
+
+        _cyclicComponent.InvokeOnTerminate();
+
+        Assert.That(_cyclicComponent.LastExecution, Is.EqualTo(DateTime.Now).Within(1).Seconds);
+    }
+
+    /// <summary>
+    /// Tests that setting the <see cref="BaseCyclicComponent.Period"/> property to an invalid value throws an <see cref="ArgumentException"/>.
+    /// </summary>
+    [Test]
+    public void Period_SetInvalidValue_ThrowsArgumentException()
+        => Assert.Throws<ArgumentException>(() => _cyclicComponent.Period = TimeSpan.Zero);
+
+    /// <summary>
+    /// A testable version of <see cref="BaseCyclicComponent"/> to expose protected methods for testing.
+    /// </summary>
+    private class TestableCyclicComponent : BaseCyclicComponent
+    {
+        public void InvokeOnInitialize() => OnInitialize();
+
+        public void InvokeOnExecute(CancellationToken cancellationToken) => OnExecute(cancellationToken);
+
+        public void InvokeOnTerminate() => OnTerminate();
+
+        // Implement the abstract members from the base class
+        public override bool CanExecute => true;
+    }
+}
---
+++ src/Test/Core/Component/BaseExecutableComponentTests.cs
@@ -0,0 +1,125 @@
+﻿// Copyright © 2024 Lionk Project
+
+using Lionk.Core.Component;
+
+namespace LionkTest.Core;
+
+/// <summary>
+/// Test class for the <see cref="BaseExecutableComponent"/> class.
+/// </summary>
+[TestFixture]
+public class BaseExecutableComponentTests
+{
+    private TestableExecutableComponent _testableComponent;
+
+    /// <summary>
+    /// Tears down the test environment.
+    /// </summary>
+    [TearDown]
+    public void TearDown()
+        => _testableComponent.Dispose();
+
+    /// <summary>
+    /// Setups the test environment.
+    /// </summary>
+    [SetUp]
+    public void Setup()
+        => _testableComponent = new TestableExecutableComponent();
+
+    /// <summary>
+    /// Tests the <see cref="BaseExecutableComponent.Execute"/> method when <see cref="BaseExecutableComponent.CanExecute"/> is false.
+    /// </summary>
+    [Test]
+    public void Execute_WhenCanExecuteIsFalse_ThrowsInvalidOperationException()
+    {
+        // Arrange
+        _testableComponent.CanExecuteSetter = false;
+
+        // Act & Assert
+        Assert.Throws<InvalidOperationException>(() => _testableComponent.Execute());
+    }
+
+    /// <summary>
+    /// Tests the <see cref="BaseExecutableComponent.Execute"/> method when the component is already running.
+    /// </summary>
+    [Test]
+    public void Execute_WhenIsRunningIsTrue_ThrowsInvalidOperationException()
+    {
+        // Arrange
+        _testableComponent.CanExecuteSetter = true;
+        _testableComponent.IsRunning = true;
+
+        // Act & Assert
+        Assert.Throws<InvalidOperationException>(() => _testableComponent.Execute());
+    }
+
+    /// <summary>
+    /// Tests the <see cref="BaseExecutableComponent.Execute"/> method when it can be executed successfully.
+    /// </summary>
+    [Test]
+    public void Execute_WhenValid_ExecutesSuccessfully()
+    {
+        // Arrange
+        _testableComponent.CanExecuteSetter = true;
+        _testableComponent.IsRunning = false;
+
+        // Act
+        _testableComponent.Execute();
+
+        // Assert
+        Assert.IsTrue(_testableComponent.OnExecuteCalled);
+        Assert.IsTrue(_testableComponent.OnTerminateCalled);
+    }
+
+    /// <summary>
+    /// Tests the <see cref="BaseExecutableComponent.Abort"/> method.
+    /// </summary>
+    [Test]
+    public void Abort_WhenCalled_CancelsToken()
+    {
+        // Arrange
+        _testableComponent.CanExecuteSetter = true;
+
+        // Act
+        _testableComponent.Abort();
+
+        // Assert
+        Assert.IsTrue(_testableComponent.TokenCancelled);
+    }
+
+    /// <summary>
+    /// A testable version of <see cref="BaseExecutableComponent"/> for exposing protected methods.
+    /// </summary>
+    private class TestableExecutableComponent : BaseExecutableComponent
+    {
+        public bool OnExecuteCalled { get; private set; }
+
+        public bool OnTerminateCalled { get; private set; }
+
+        public bool TokenCancelled { get; private set; }
+
+        public new bool IsRunning { get; set; }
+
+        public override bool CanExecute => CanExecuteSetter;
+
+        public bool CanExecuteSetter { get; set; }
+
+        protected override void OnExecute(CancellationToken cancellationToken)
+        {
+            OnExecuteCalled = true;
+            base.OnExecute(cancellationToken);
+        }
+
+        protected override void OnTerminate()
+        {
+            OnTerminateCalled = true;
+            base.OnTerminate();
+        }
+
+        public override void Abort()
+        {
+            TokenCancelled = true;
+            base.Abort();
+        }
+    }
+}
