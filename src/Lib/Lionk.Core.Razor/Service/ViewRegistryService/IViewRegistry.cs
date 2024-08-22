6dd673ce0bf795d7e5f1d3cca1831086c5dc09b9
CoJaques <colin.jaques@heig-vd.ch>
2024-08-22 21:42:05 +0200

    Add DynamicComponentWrapper and view service

---
+++ src/Lib/Lionk.Core.Razor/Service/ViewRegistryService/IViewRegistry.cs
@@ -0,0 +1,28 @@
+﻿// Copyright © 2024 Lionk Project
+
+namespace Lionk.Core.View;
+
+/// <summary>
+/// Interface which define the service to register the active views.
+/// </summary>
+public interface IViewRegistry
+{
+    /// <summary>
+    /// Method used to register a view.
+    /// </summary>
+    /// <param name="viewInstance">The view instance.</param>
+    void Register(object viewInstance);
+
+    /// <summary>
+    /// Method used to unregister a view.
+    /// </summary>
+    /// <param name="viewInstance">The view instance.</param>
+    void Unregister(object viewInstance);
+
+    /// <summary>
+    /// Method used to check if an instance of the view is active.
+    /// </summary>
+    /// <typeparam name="T">The type of the view.</typeparam>
+    /// <returns>True if an instance of this type exist.</returns>
+    bool HasActiveViews<T>();
+}
---
+++ src/Lib/Lionk.Core.Razor/Service/ViewRegistryService/ViewRegistryService.cs
@@ -0,0 +1,23 @@
+﻿// Copyright © 2024 Lionk Project
+
+namespace Lionk.Core.View;
+
+/// <summary>
+/// Service which register the active views.
+/// </summary>
+public class ViewRegistryService : IViewRegistry
+{
+    private readonly HashSet<object> _activeViews = [];
+
+    /// <inheritdoc cref="IViewRegistry"/>
+    public void Register(object viewInstance)
+        => _activeViews.Add(viewInstance);
+
+    /// <inheritdoc cref="IViewRegistry"/>
+    public void Unregister(object viewInstance)
+        => _activeViews.Remove(viewInstance);
+
+    /// <inheritdoc cref="IViewRegistry"/>
+    public bool HasActiveViews<T>()
+        => _activeViews.Any(view => view is T);
+}
