2cc03038dd6101383d64f69b11394211f6fcb152
Alexandre Iorio <alexandre.iorio@heig-vd.ch>
2024-08-22 21:04:02 +0200

    work

---
+++ src/PublishLinuxArm64.bat
@@ -0,0 +1,19 @@
+﻿@echo off
+rem Define variables
+set CONFIGURATION=Debug
+set RUNTIME_IDENTIFIER=linux-arm64
+set TARGET_FRAMEWORK=net8.0
+set PUBLISH_DIR=output
+set PROJECT_NAME=LionkApp
+
+rem Create project-specific output directory
+set PROJECT_OUTPUT_DIR=%PUBLISH_DIR%\%PROJECT_NAME%
+
+rem Publish the application
+dotnet publish -c %CONFIGURATION% -r %RUNTIME_IDENTIFIER% -o %PROJECT_OUTPUT_DIR% --self-contained
+
+rem Check if the publish was successful
+if %ERRORLEVEL% NEQ 0 (
+    echo Publication failed!
+    exit /b %ERRORLEVEL%
+)
