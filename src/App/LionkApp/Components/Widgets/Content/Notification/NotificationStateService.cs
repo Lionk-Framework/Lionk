12e429d9068211169949321fd9d8528ca829c0b0
Theodros <theodros.mulugeta@runciter.ch>
2024-08-26 21:24:10 +0200

    Addin badge

---
+++ src/App/LionkApp/Components/Widgets/Content/Notification/NotificationStateService.cs
@@ -0,0 +1,20 @@
+﻿// Copyright © 2024 Lionk Project
+
+using Lionk.Notification;
+
+/// <summary>
+///     This class represents a service that tracks the state of notifications.
+/// </summary>
+public class NotificationStateService
+{
+    /// <summary>
+    ///     This event is triggered when a notification is received.
+    /// </summary>
+    public event Action? OnNotificationReceived;
+
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="NotificationStateService"/> class
+    ///     and subscribes to the notification sent event.
+    /// </summary>
+    public NotificationStateService() => NotificationService.NotificationSent += (sender, args) => OnNotificationReceived?.Invoke();
+}
