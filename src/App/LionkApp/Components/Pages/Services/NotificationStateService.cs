ea9c498e9db63473afefb962d3a170d530c590ed
Theodros <theodros.mulugeta@runciter.ch>
2024-08-26 21:36:51 +0200

    Adding counter

--- src/App/LionkApp/Components/Pages/Services/NotificationStateService.cs
+++ src/App/LionkApp/Components/Pages/Services/NotificationStateService.cs
@@ -13,8 +13,41 @@ public class NotificationStateService
     public event Action? OnNotificationReceived;
 
     /// <summary>
+    ///     This event is triggered when the badge content changes.
+    /// </summary>
+    public event Action? OnBadgeContentChanged;
+
+    private int _badgeContent;
+
+    /// <summary>
+    ///     Gets or sets the badge content.
+    /// </summary>
+    public int BadgeContent
+    {
+        get => _badgeContent;
+        set
+        {
+            if (_badgeContent != value)
+            {
+                _badgeContent = value;
+                OnBadgeContentChanged?.Invoke();
+            }
+        }
+    }
+
+    /// <summary>
     ///     Initializes a new instance of the <see cref="NotificationStateService"/> class
     ///     and subscribes to the notification sent event.
     /// </summary>
     public NotificationStateService() => NotificationService.NotificationSent += (sender, args) => OnNotificationReceived?.Invoke();
+
+    /// <summary>
+    /// Resets the badge content to zero.
+    /// </summary>
+    public void ResetBadgeContent() => BadgeContent = 0;
+
+    /// <summary>
+    /// Increments the badge content by one.
+    /// </summary>
+    public void IncrementBadgeContent() => BadgeContent++;
 }
