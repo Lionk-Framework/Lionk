465eeafb6a0771e7ae0581a4494830e80c7e4d8e
Alexandre Iorio <alexandre.iorio@heig-vd.ch>
2024-08-28 17:35:49 +0200

    edit

--- .github/workflows/scripts/app_scripts/restore_release.py
+++ .github/workflows/scripts/app_scripts/restore_release.py
@@ -13,13 +13,11 @@ tag = f"{app_name}_{newversion}"
 
 print(f"Deleting tag {tag}")
 
-# Delete the tag locally
-subprocess.run(['git', 'tag', '-d', tag], check=True)
+# Delete the release
+subprocess.run(['gh', 'release', 'delete', tag, '--yes'], check=True)
 
 # Delete the tag remotely
 subprocess.run(['git', 'push', '--delete', 'origin', tag], check=True)
 
-# Delete the release
-subprocess.run(['gh', 'release', 'delete', tag, '--yes'], check=True)
 
 
