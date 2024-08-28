aa5455255fefe17fd890e6c6a44c0ba813df2f81
Alexandre Iorio <alexandre.iorio@heig-vd.ch>
2024-08-28 17:46:35 +0200

    edit

--- .github/workflows/scripts/app_scripts/publish_package.py
+++ .github/workflows/scripts/app_scripts/publish_package.py
@@ -24,7 +24,7 @@ with open('newversion.txt', 'r') as file:
 
 print(f"Publishing {app_name} as version {newversion}")
 
-run_command(['cd', sln_path])
+dockerfile = os.path.join(sln_path, 'Dockerfile')
 
 # Construire l'image Docker avec une étiquette de description
 run_command(['docker', 'build', '-t', f"{docker_registry}/{app_name.lower()}:{newversion}", '-t',f"{docker_registry}/{app_name.lower()}:latest", '.'])	
